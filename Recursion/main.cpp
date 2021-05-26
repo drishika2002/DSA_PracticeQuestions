44a289a39ea523ef40142b2286d18ad15d9259a2
Drishika Chauhan <76254057+drishika2002@users.noreply.github.com>
2021-05-26 23:13:35 +0530

    counting digits code added

---
+++ Recursion/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int count = 0;
+
+int num_Of_digits(long long int num)
+{
+    if(num != 0)
+    {
+        count++;
+        num_Of_digits(num/10);
+    }
+    return count;
+}
+
+int main()
+{
+    long long int n,res;
+    cin >> n;
+
+    res = num_Of_digits(n);
+    cout << res << endl;
+    return 0;
+}
+
