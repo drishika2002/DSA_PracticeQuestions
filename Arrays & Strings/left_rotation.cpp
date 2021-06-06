4ab81b0f60f14d0774f020d5d01c60bc4c429825
Drishika Chauhan <76254057+drishika2002@users.noreply.github.com>
2021-06-06 17:46:25 +0530

    Create left_rotation.cpp

---
+++ Arrays & Strings/left_rotation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n, d;
+    cin >> n >> d;
+    int a[n];
+    for(int i = 0; i < n;i++)
+    {
+        cin >> a[i];
+    }
+    
+    for(int i = 0; i < n;i++)
+    {
+		cout<<a[(i+d)%n]<<" ";
+    }
+
+    return 0;
+}
