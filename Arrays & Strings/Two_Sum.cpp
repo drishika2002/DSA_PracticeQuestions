09744b3fa4fde08341fddfdaae33b3992f6834be
Drishika Chauhan <76254057+drishika2002@users.noreply.github.com>
2021-06-16 00:28:36 +0530

    Create Two_Sum.cpp

---
+++ Arrays & Strings/Two_Sum.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        vector<int> ans(2);
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+         }
+        return ans;
+    }
+};
+
+/* 
+Time complexity : O(n^2)
+Space Complexity: O(1)
+*/
